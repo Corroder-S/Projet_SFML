f55cffbb2cd3a68df8127883b1f0d5fd86c2ec6a
sbrossard <sacha.brossard22@gmail.com>
2024-11-25 10:58:17 +0100

    Textures et interface utilisateur

--- main.cpp
+++ main.cpp
@@ -11,35 +11,89 @@ bool meteor3b = false;
 bool meteor4b = false;
 bool canShoot = true;
 bool shooting = false;
+bool gameover = false;
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Fenêtre SFML");
+    Clock global;
     Clock clock;
     Clock shoot;
+    int scorecount;
     float temps = 0;
     float cd = 0;
     srand(time(0));
+
+    Font font;
+    if (!font.loadFromFile("C:\\Users\\sbrossard\\source\\repos\\Corroder-S\\Projet_SFML\\scifie.ttf")) {
+        return -1;
+    }
+
+    Text aff_score("Score : 0", font, 50);
+    aff_score.setPosition(800, 90);
+
+    Text aff_can_shoot("FIRE", font, 20);
+    aff_can_shoot.setPosition(1522, 112);
+    aff_can_shoot.setFillColor(Color::Black);
+
+    Texture life;
+    if (!life.loadFromFile("C:\\Users\\sbrossard\\source\\repos\\Corroder-S\\Projet_SFML\\coeur.png")){
+        return -1;}
+
+    Texture meteor;
+    if (!meteor.loadFromFile("C:\\Users\\sbrossard\\source\\repos\\Corroder-S\\Projet_SFML\\meteor.png")) {
+        return -1;
+    }
+
+    Texture spacecraft;
+    if (!spacecraft.loadFromFile("C:\\Users\\sbrossard\\source\\repos\\Corroder-S\\Projet_SFML\\spacecraft.png")) {
+        return -1;
+    }
+
+    Texture background;
+    if (!background.loadFromFile("C:\\Users\\sbrossard\\source\\repos\\Corroder-S\\Projet_SFML\\space.jpg")) {
+        return -1;
+    }
+
+    Sprite backgroundimage(background);
+
+    CircleShape coeur(50);
+    coeur.setPosition(Vector2f(100, 75));
+    coeur.setTexture(&life);
+
+
     CircleShape meteor1(70);
     meteor1.setPosition(2000, 100);
+    meteor1.setTexture(&meteor);
 
     CircleShape meteor2(70);
     meteor2.setPosition(2000, 300);
+    meteor2.setTexture(&meteor);
 
     CircleShape meteor3(70);
     meteor3.setPosition(2000, 500);
+    meteor3.setTexture(&meteor);
 
     CircleShape meteor4(70);
     meteor4.setPosition(2000, 700);
+    meteor4.setTexture(&meteor);
 
     RectangleShape vaisseau(Vector2f(200, 50));
     vaisseau.setPosition(200, 500);
+    vaisseau.setTexture(&spacecraft);
 
     RectangleShape missile(Vector2f(2000, 20));
     missile.setFillColor(Color::Blue);
+
+    RectangleShape can_shoot_indicator(Vector2f(100, 100));
+    can_shoot_indicator.setPosition(1500, 75);
+    can_shoot_indicator.setFillColor(Color::Green);
                     
     window.setFramerateLimit(60);
 
     while (window.isOpen()) {
+        scorecount = global.getElapsedTime().asSeconds() * 100;
+        String score = to_string(scorecount);
+        aff_score.setString("Score : " + score);
         int x = vaisseau.getPosition().x;
         int y = vaisseau.getPosition().y + 15;
         missile.setPosition(Vector2f(x, y));
@@ -114,12 +168,13 @@ int main() {
 
     cd = shoot.getElapsedTime().asSeconds();
 
-    if (cd >= 5 and canShoot == false){
+    if (cd >= 3 and canShoot == false){
         canShoot = true;
     }
 
     temps = clock.getElapsedTime().asSeconds();
     
+
 
     if (temps >= 2.0f)
     {
@@ -135,26 +190,48 @@ int main() {
         clock.restart();
     
     }
-    if (vies == 0) {
-        window.close();
-        }
-        window.clear();
-        if (meteor1b)
-            window.draw(meteor1);
-        if (meteor2b)
-            window.draw(meteor2);
-        if (meteor3b)
-            window.draw(meteor3);
-        if (meteor4b)
-            window.draw(meteor4);
-        if (shooting){
-            window.draw(missile);
-            shooting = false;}
-        window.draw(vaisseau);
-        window.display();
-        temps++;
+    if (!gameover){
+        if (vies == 0) {
+            window.close();
+            }
+            window.clear();
+            window.draw(backgroundimage);
+            window.draw(aff_score);
+            window.draw(coeur);
+              if (vies >= 2) {
+                  coeur.setPosition(200, 75);
+                  window.draw(coeur);
+                  if (vies == 3) {
+                      coeur.setPosition(300, 75);
+                      window.draw(coeur);
+                    }
+                }
+            coeur.setPosition(100, 75);
+            if (canShoot) {
+                window.draw(can_shoot_indicator);
+            }
+            else {
+                can_shoot_indicator.setFillColor(Color::Red);
+                window.draw(can_shoot_indicator);
+                can_shoot_indicator.setFillColor(Color::Green);
+            }
+            window.draw(aff_can_shoot);
+            if (meteor1b)
+                window.draw(meteor1);
+            if (meteor2b)
+                window.draw(meteor2);
+            if (meteor3b)
+                window.draw(meteor3);
+            if (meteor4b)
+                window.draw(meteor4);
+            if (shooting){
+                window.draw(missile);
+                shooting = false;}
+            window.draw(vaisseau);
+            window.display();
+            temps++;
         
+        }
     }
-
     return 0;
 }
